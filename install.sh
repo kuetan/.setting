2c96ffd3bc25280c5f6e19864c38e1ba9d60781e
kuetan <kuroawa.yk@gmail.com>
2018-04-11 20:42:03 +0900

    remove sudo

--- install.sh
+++ install.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
-sudo apt-get install git gcc make openssl libssl-dev libbz2-dev libreadline-dev libsqlite3-dev
-sudo git clone git://github.com/yyuu/pyenv.git $HOME/.pyenv
+apt-get install git gcc make openssl libssl-dev libbz2-dev libreadline-dev libsqlite3-dev
+git clone git://github.com/yyuu/pyenv.git $HOME/.pyenv
 source $HOME/.setting/zsh/.zshrc
 pyenv intall 3.6.4
 pyenv global 3.6.4

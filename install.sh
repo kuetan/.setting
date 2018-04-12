2dae4e28700377c5b91cdc06dccbe41e35dc762d
kurosawa-yuya <kurosawa.yk@gmail.com>
2018-04-12 10:45:37 +0000

    modify pyenv

--- install.sh
+++ install.sh
@@ -3,6 +3,7 @@
 apt-get install git gcc make openssl libssl-dev libbz2-dev libreadline-dev libsqlite3-dev
 git clone git://github.com/yyuu/pyenv.git $HOME/.pyenv
 source $HOME/.setting/zsh/.zshrc
-$HOME/.pyenv/bin/pyenv intall 3.6.4
-$HOME/.pyenv/bin/pyenv global 3.6.4
+echo "check_certificate = off" >> /etc/wgetrc
+$HOME/.pyenv/bin/pyenv install 3.6.5
+$HOME/.pyenv/bin/pyenv global 3.6.5
 $HOME/.pyenv/bin/pyenv rehash

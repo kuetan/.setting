fc85d4862fa8853dc3173f9fcdae129222bd29c4
kurosawa <kurosawa.yk@gmail.com>
2018-04-21 05:36:52 +0000

    add g++

--- install.sh
+++ install.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-apt-get install -y git emacs zsh gcc make openssl libssl-dev libbz2-dev libreadline-dev libsqlite3-dev
+apt-get install -y git g++ wget emacs zsh gcc make openssl libssl-dev libbz2-dev libreadline-dev libsqlite3-dev
 git clone git://github.com/yyuu/pyenv.git $HOME/.pyenv
 $HOME/.setting/link.sh
 source $HOME/.setting/zsh/.zshrc
@@ -8,3 +8,4 @@ echo "check_certificate = off" >> /etc/wgetrc
 $HOME/.pyenv/bin/pyenv install 3.6.5
 $HOME/.pyenv/bin/pyenv global 3.6.5
 $HOME/.pyenv/bin/pyenv rehash
+zsh

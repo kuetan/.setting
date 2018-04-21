a8eaed629c72736c4f4e4c27ca6c342e7cbc6883
kue <20365670+kuetan@users.noreply.github.com>
2018-04-21 13:45:02 +0900

    do link.sh

--- install.sh
+++ install.sh
@@ -1,7 +1,8 @@
 #!/usr/bin/env bash
 
-apt-get install -y git gcc make openssl libssl-dev libbz2-dev libreadline-dev libsqlite3-dev
+apt-get install -y git emacs zsh gcc make openssl libssl-dev libbz2-dev libreadline-dev libsqlite3-dev
 git clone git://github.com/yyuu/pyenv.git $HOME/.pyenv
+$HOME/.setting/link.sh
 source $HOME/.setting/zsh/.zshrc
 echo "check_certificate = off" >> /etc/wgetrc
 $HOME/.pyenv/bin/pyenv install 3.6.5

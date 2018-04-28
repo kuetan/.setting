82c76defa31a89d2e6992b9e62122729a70266de
kuetan <kuroawa.yk@gmail.com>
2018-04-28 21:57:02 +0900

    add update tensorflow

--- update-tensorflow.sh
+++ update-tensorflow.sh
@@ -1,36 +1,26 @@
-# install bazel
-#apt-get install pkg-config zip g++ zlib1g-dev unzip
-#wget https://github.com/bazelbuild/bazel/releases/download/0.12.0/bazel-0.12.0-installer-linux-x86_64.sh
-#chmod u+x bazel-0.12.0-installer-linux-x86_64.sh
-#./bazel-0.12.0-installer-linux-x86_64.sh --user
-#export PATH="$PATH:$HOME/bin"
+#!/bin/bash
+# Patrick Wieschollek
 
-apt-get install -y openjdk-8-jdk
-add-apt-repository ppa:webupd8team/java -y
-apt-get update && sudo apt-get install -y oracle-java8-installer
-echo "deb [arch=amd64] http://storage.googleapis.com/bazel-apt stable jdk1.8" | tee /etc/apt/sources.list.d/bazel.list
-curl https://bazel.build/bazel-release.pub.gpg | sudo apt-key add -
+# =============================================================
+# UPDATE SOURCE
+# =============================================================
+git checkout -- .
+git pull origin master
 
-apt-get update && apt-get install -y bazel
-apt-get upgrade bazel
 
-apt install python3 python3-dev python3-pip
-pip install six numpy wheel
-
-# protobuf
-apt-get install libprotobuf-dev protobuf-compiler
-
-# install tensorflow
+python_version = python3
+tensorflow_version = 1.8.0
 
 echo "build TensorFlow for Python version:", ${python_version}
 
 # =============================================================
 # CONFIGURATION
 # =============================================================
-TF_ROOT=/opt/tensorflow
+TF_ROOT=/opt/tensorflow-${tensorflow_version}
 
 cd $TF_ROOT
-export PYTHON_BIN_PATH=$(which python)
+
+export PYTHON_BIN_PATH=$(which ${python_version})
 export PYTHON_LIB_PATH="$($PYTHON_BIN_PATH -c 'import site; print(site.getsitepackages()[0])')"
 export PYTHONPATH=${TF_ROOT}/lib
 export PYTHON_ARG=${TF_ROOT}/lib
@@ -40,10 +30,10 @@ export CUDNN_INSTALL_PATH=/usr/local/cuda
 export TF_NEED_GCP=0
 export TF_NEED_CUDA=1
 export TF_CUDA_VERSION="$($CUDA_TOOLKIT_PATH/bin/nvcc --version | sed -n 's/^.*release \(.*\),.*/\1/p')"
-#export TF_CUDA_COMPUTE_CAPABILITIES=6.1,5.2,3.5
+export TF_CUDA_COMPUTE_CAPABILITIES=61
 export TF_NEED_HDFS=0
 export TF_NEED_OPENCL=0
-export TF_NEED_JEMALLOC=0
+export TF_NEED_JEMALLOC=1
 export TF_ENABLE_XLA=0
 export TF_NEED_VERBS=0
 export TF_CUDA_CLANG=0
@@ -51,8 +41,10 @@ export TF_CUDNN_VERSION="$(sed -n 's/^#define CUDNN_MAJOR\s*\(.*\).*/\1/p' $CUDN
 export TF_NEED_MKL=0
 export TF_DOWNLOAD_MKL=0
 export TF_NEED_MPI=0
+
 export GCC_HOST_COMPILER_PATH=$(which gcc)
 export CC_OPT_FLAGS="-march=native"
+
 
 # =============================================================
 # BUILD NEW VERSION
@@ -66,3 +58,5 @@ bazel clean
 bazel build  -c opt --copt=-mfpmath=both --copt=-msse4.2 --config=cuda //tensorflow/tools/pip_package:build_pip_package
 # build c++ library
 bazel build  -c opt --copt=-mfpmath=both --copt=-msse4.2 --config=cuda  tensorflow:libtensorflow_cc.so
+# build TF pip package
+bazel-bin/tensorflow/tools/pip_package/build_pip_package ${TF_ROOT}/pip/tensorflow_pkg

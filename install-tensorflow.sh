53c016a1524bcdf982eac4f65311a61dfa933563
kuetan <kuroawa.yk@gmail.com>
2018-04-29 00:16:02 +0900

    add update tensorflow

--- install-tensorflow.sh
+++ install-tensorflow.sh
@@ -13,20 +13,10 @@ echo "deb [arch=amd64] http://storage.googleapis.com/bazel-apt stable jdk1.8" | 
 curl https://bazel.build/bazel-release.pub.gpg | sudo apt-key add -
 
 apt-get update && apt-get install -y bazel
-apt-get upgrade bazel
+apt-get -y upgrade bazel
 
 apt install -y python3 python3-dev python3-pip
 pip install -y six numpy wheel
 
 # protobuf
 apt install -y libprotobuf-dev protobuf-compiler
-
-# install tensorflow
-VERSION=1.8.0
-wget https://github.com/tensorflow/tensorflow/archive/v${VERSION}.tar.gz -P /opt/
-cd /opt/ && tar zxvf v${VERSION}.tar.gz
-#echo "build TensorFlow for Python version:", ${python_version}
-
-# =============================================================
-# CONFIGURATION
-# =============================================================

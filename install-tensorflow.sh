4d09663f2ce6805f95586d5ffa24d310af2e2fda
kuetan <kuroawa.yk@gmail.com>
2018-04-29 00:17:53 +0900

    add update tensorflow

--- install-tensorflow.sh
+++ install-tensorflow.sh
@@ -10,7 +10,7 @@ apt-get install -y openjdk-8-jdk
 add-apt-repository ppa:webupd8team/java -y
 apt-get update && sudo apt-get install -y oracle-java8-installer
 echo "deb [arch=amd64] http://storage.googleapis.com/bazel-apt stable jdk1.8" | tee /etc/apt/sources.list.d/bazel.list
-curl https://bazel.build/bazel-release.pub.gpg | sudo apt-key add -
+curl https://bazel.build/bazel-release.pub.gpg | apt-key add -
 
 apt-get update && apt-get install -y bazel
 apt-get -y upgrade bazel
